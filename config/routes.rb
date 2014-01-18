3efa8d599d1e2f88fbd0848748a68c4b41a4d802
heenaikhan786 <heenaikhan934@gmail.com>
2014-01-18 15:42:35 -0800

    Clean up the pins scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Command::Application.routes.draw do
+  resources :pins
+
   devise_for :users
   root "page#command"
   get "home" => "page#home" #create home path
+  
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
